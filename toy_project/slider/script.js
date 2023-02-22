1632d68066858557f35fba35de85b6a66bb88a9c
unknown <joena0603@gmail.com>
2023-02-22 17:57:00 +0900

    toy project : 슬라이더 수렁텅이에 빠졌어 살려줘

--- toy_project/slider/script.js
+++ toy_project/slider/script.js
@@ -17,49 +17,74 @@ $(document).ready(function(){
 
 
 // 리팩토링 하자
-let containerItem = $(".containerItem");
-let slideImg = $(".slideImg");
-let imgLeng = $(".slideImg").length-1; // 현재 3
-let moveWidth = -100;
-let nowImg = 0;
+let _$containerItem = $(".containerItem");
+let _$imgLeng = $(".slideImg").length-1; // 현재 3
 
-// 이미지 클론
-function doClone(){
-    let firstImg = slideImg.eq(0);
-    let lastImg = slideImg.eq(imgLeng + 1);
-    
-    slideImg.append("firstImg")
-    slideImg.prepend("lastImg")
+
+// transform 사용
+// function doNext(){
+//   let moveWidth = -100;
+//   let nowImg = 0;
+//     if(nowImg < _$imgLeng){
+//         _$containerItem.css({"transform":"translateX("+ moveWidth +"vw)"})
+//         moveWidth -= 100;
+//         nowImg += 1;
+//     } else if (nowImg >= _$imgLeng){
+//         _$containerItem.css({"transform":"translateX(0vw)"})
+//         moveWidth = -100;
+//         nowImg = 0;
+//     }
+//     console.log("nowImg -> " + nowImg, "imgLeng -> " + _$imgLeng)
+// }
+
+function getSlideImg(){
+    let _$slideImg = $(".slideImg");
+    return _$slideImg;
 }
 
-doClone();
+// paganation array 짜기
 
-function doNext(){
-    if(nowImg < imgLeng){
-        containerItem.css({"transform":"translateX("+ moveWidth +"vw)"})
-        moveWidth -= 100;
-        nowImg += 1;
-    } else if (nowImg >= imgLeng){
-        containerItem.css({"transform":"translateX(0vw)"})
-        moveWidth = -100;
-        nowImg = 0;
+
+// nav 변화주기
+// data num이 마지막꺼면, 처음껄로 돌리기, 처음꺼면, 마지막껄로 돌리기(무한으로 돌려)
+let num = 0;
+function moveNav(){
+    if(num < _$imgLeng ){
+        $(".btn").eq(num).removeClass("btnOn");
+        $(".btn").eq(num+1).addClass("btnOn")
+
+        num += 1;
+    } else if(num > _$imgLeng){ 
+        $(".btn").eq(_$imgLeng).removeClass("btnOn");
+        $(".btn").eq(0).addClass("btnOn")
+
+        num = 0
     }
-    console.log("nowImg -> " + nowImg, "imgLeng -> " + imgLeng)
+    
+    console.log("num -> " + num + " / _$imgLeng -> " + _$imgLeng)
 }
 
-function doPrev(){
-
-}
-
-
-
+//doPrev
 $(".prev").click(function(){
-    doPrev();
+    let _$getSlideImg = getSlideImg();
+    $(_$getSlideImg[_$imgLeng]).prependTo($("ul"))
+    
+
 })
 
 //다음
-$(".next").click(function(){
-    doNext();
-})
+$(".next").on("click", function(){
+    let _$getSlideImg = getSlideImg();
+    $(_$getSlideImg[0]).appendTo($("ul"));
+
+    moveNav();
+});
+
+// $(".next").click(function(){
+//     let _$getSlideImg = getSlideImg();
+//     $(_$getSlideImg[0]).appendTo($("ul"));
+
+//     moveNav();
+// })
 
 });
