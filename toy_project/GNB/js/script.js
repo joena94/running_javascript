ab99aba6aecdbe548d93ca000ee5d8ab0b04de0d
unknown <joena0603@gmail.com>
2023-04-07 17:54:01 +0900

    maek responsive GNB

--- toy_project/GNB/js/script.js
+++ toy_project/GNB/js/script.js
@@ -2,7 +2,14 @@ $(function(){
     (function(){
         let _$header = $("header")
 
-        console.log(_$header.scrollTop())
+        $(window).scroll(function(){
+            let height = $(document).scrollTop();
+            if(height > 170){
+                $("header").addClass("fixed");
+            } else if(height < 170) {
+                $("header").removeClass("fixed");
+            }
+        })
 
         $(".gnb > .gnb_top > li").on("focusin mouseenter click", function(e){
             $(this).find("a").addClass("on");
@@ -16,5 +23,12 @@ $(function(){
             $(".depth2").removeClass("active");
             $("header").removeClass("active");     // 백그라운드 
         });
+
+        $(window).resize(function(){
+            if(window.innerWidth < 1023){
+                // $(".gnb > .gnb_top > li").find("a").removeClass("on");
+                // $(".gnb > .gnb_top > li").find(".depth2").removeClass("on");
+            }   
+        })
     })();
 });
