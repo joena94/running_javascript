3b4b434f4bc3e766e0ed2a0f94344a87317bfbc3
unknown <joena0603@gmail.com>
2023-04-10 17:02:58 +0900

    add code GNB

--- toy_project/GNB/js/script.js
+++ toy_project/GNB/js/script.js
@@ -11,24 +11,44 @@ $(function(){
             }
         })
 
-        $(".gnb > .gnb_top > li").on("focusin mouseenter click", function(e){
-            $(this).find("a").addClass("on");
-            $(this).find(".depth2").addClass("on");
-            $(".depth2").addClass("active");
-            $("header").addClass("active");     // 백그라운드 
-        });
-        $(".gnb > .gnb_top > li").on("mouseleave", function(e){
-            $(this).find("a").removeClass("on");
-            $(this).find(".depth2").removeClass("on");
-            $(".depth2").removeClass("active");
-            $("header").removeClass("active");     // 백그라운드 
-        });
+        // $(".gnb > .gnb_top > li").on("focusin mouseenter click", function(e){
+        //     $(this).find("a").addClass("on");
+        //     $(this).find(".depth2").addClass("on");
+        //     $(".depth2").addClass("active");
+        //     $("header").addClass("active");     // 백그라운드 
+        // });
+        // $(".gnb > .gnb_top > li").on("mouseleave", function(e){
+        //     $(this).find("a").removeClass("on");
+        //     $(this).find(".depth2").removeClass("on");
+        //     $(".depth2").removeClass("active");
+        //     $("header").removeClass("active");     // 백그라운드 
+        // });
 
-        $(window).resize(function(){
-            if(window.innerWidth < 1023){
-                // $(".gnb > .gnb_top > li").find("a").removeClass("on");
-                // $(".gnb > .gnb_top > li").find(".depth2").removeClass("on");
-            }   
-        })
+        // let ww = $(window).width();
+        // let timer = null;
+        // let sec = 300;;
+
+        // $(window).on("resize", function(){
+        //     clearTimeout(timer);
+        //     timer = setTimeout(function(){
+        //         ww = $(window).width();
+        //         if(ww < 1023){
+        //             let _$responsiveDepth1 = $(".depth1");
+        //             console.log()
+        //             _$responsiveDepth1.children().first().addClass("active");
+        //             _$responsiveDepth1.children(".depth2").first().addClass("active");
+        //             _$responsiveDepth1.on("click", function(e){
+        //                 _$responsiveDepth1.find("a").removeClass("active");
+        //                 _$responsiveDepth1.children(".depth2").removeClass("active");
+        //                 $(this).children("a").addClass("active");
+        //                 $(this).children(".depth2").addClass("active");
+
+                        
+        //             })
+                    
+        //         }
+        //     })
+        // });
+
     })();
 });
